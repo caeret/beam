47b03a496ff45e6c000aa7de69485c5b50c54958
Ren Hao <renhao2k@gmail.com>
2017-08-08 18:41:47 +0800

    add basic funcs for request.

---
+++ request.go
@@ -0,0 +1,122 @@
+package beam
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"strconv"
+)
+
+var (
+	ErrFormat = errors.New("invalid format")
+	crlf      = []byte{'\r', '\n'}
+)
+
+// Request represent the redis request command.
+type Request [][]byte
+
+func (r Request) String() string {
+	return `"` + string(bytes.Join(r, []byte{' '})) + `"`
+}
+
+func (r Request) Raw() string {
+	raw := "*" + strconv.Itoa(len(r)) + string(crlf)
+	for _, elem := range r {
+		raw += "$" + strconv.Itoa(len(elem)) + string(crlf)
+		raw += string(elem) + string(crlf)
+	}
+	return raw
+}
+
+// ReadRequest read a request from the io.Reader.
+func ReadRequest(reader io.Reader) (request Request, err error) {
+	bReader := bufio.NewReader(reader)
+	n, err := readArgsCount(bReader)
+	if err != nil {
+		return
+	}
+
+	request = make(Request, n)
+
+	for i := 0; i < n; i++ {
+		var argLength int
+		argLength, err = readArgLength(bReader)
+		if err != nil {
+			return
+		}
+		var arg []byte
+		arg, err = readArgLine(argLength, bReader)
+		if err != nil {
+			return
+		}
+
+		request[i] = arg
+	}
+
+	return
+}
+
+// readArgsCount read arguments count from reader.
+func readArgsCount(reader *bufio.Reader) (n int, err error) {
+	argsCount, err := readPrefixedLine('*', reader)
+	if err != nil {
+		return
+	}
+	n, err = strconv.Atoi(string(argsCount))
+	return
+}
+
+// readArgLength read the length of argument from reader.
+func readArgLength(reader *bufio.Reader) (n int, err error) {
+	argLength, err := readPrefixedLine('$', reader)
+	if err != nil {
+		return
+	}
+	n, err = strconv.Atoi(string(argLength))
+	return
+}
+
+// readPrefixedLine read the line starts by the specified prefix and ends with crlf.
+func readPrefixedLine(prefix byte, reader *bufio.Reader) (line []byte, err error) {
+	b, err := reader.ReadByte()
+	if err != nil {
+		return
+	}
+	if b != prefix {
+		err = ErrFormat
+		return
+	}
+	line, err = reader.ReadBytes('\n')
+	if err != nil {
+		return
+	}
+	if !bytes.HasSuffix(line, crlf) {
+		err = ErrFormat
+		return
+	}
+	line = line[:len(line)-2]
+	return
+}
+
+// readArgLine read the line with specified length.
+func readArgLine(length int, reader *bufio.Reader) (line []byte, err error) {
+	line = make([]byte, length+2)
+	var (
+		n        int
+		currentN int
+	)
+	for n < length {
+		currentN, err = reader.Read(line[n:])
+		if err != nil {
+			return
+		}
+		n += currentN
+	}
+	if !bytes.HasSuffix(line, crlf) {
+		err = ErrFormat
+		return
+	}
+	line = line[:length]
+	return
+}
---
+++ request_test.go
@@ -0,0 +1,49 @@
+package beam
+
+import (
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadRequest(t *testing.T) {
+	assert := assert.New(t)
+	var (
+		reader io.Reader
+		req    Request
+		err    error
+	)
+	reader = strings.NewReader("*1\r\n$4\r\nPING\r\n")
+	req, err = ReadRequest(reader)
+	assert.Nil(err)
+	assert.Equal(req, Request{[]byte("PING")})
+
+	reader = strings.NewReader("*2\r\n$3\r\nGET\r\n$3\r\nfoo\r\n")
+	req, err = ReadRequest(reader)
+	assert.Nil(err)
+	assert.Equal(req, Request{[]byte("GET"), []byte("foo")})
+
+	reader = strings.NewReader("*2\r\nhello")
+	req, err = ReadRequest(reader)
+	assert.Error(ErrFormat, err)
+}
+
+func TestRequest_String(t *testing.T) {
+	assert := assert.New(t)
+	req1 := Request{[]byte("GET"), []byte("bar")}
+	assert.Equal(req1.String(), `"GET bar"`)
+
+	req2 := Request{[]byte("PING")}
+	assert.Equal(req2.String(), `"PING"`)
+}
+
+func TestRequest_Raw(t *testing.T) {
+	assert := assert.New(t)
+	req1 := Request{[]byte("GET"), []byte("bar")}
+	assert.Equal(req1.Raw(), "*2\r\n$3\r\nGET\r\n$3\r\nbar\r\n")
+
+	req2 := Request{[]byte("PING")}
+	assert.Equal(req2.Raw(), "*1\r\n$4\r\nPING\r\n")
+}
